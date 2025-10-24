0edac274effec91d436176a96802c45b82d73b21
server308 <sinijvitalik@gmail.com>
2025-10-24 17:19:14 +0300

    Project initialization

---
+++ user_service/src/main/java/com/food_del_pltfrm/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.food_del_pltfrm.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
