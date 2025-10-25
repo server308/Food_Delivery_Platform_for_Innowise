cf9e180e5bfd1e448b8a9695657db2c34c9ebf11
server308 <sinijvitalik@gmail.com>
2025-10-25 16:53:26 +0300

    Creating AddressRepository and RoleRepository

---
+++ user_service/src/main/java/com/food_del_pltfrm/user_service/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.food_del_pltfrm.user_service.repositories;
+
+
+import com.food_del_pltfrm.user_service.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
